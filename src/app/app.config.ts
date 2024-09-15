1c63a602f0c4a87499286e9a4c8a8bf66376e19b
Your Name <you@example.com>
2024-09-15 11:02:31 +0200

    added first user to firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'join-angular-708af',
+          appId: '1:953154306638:web:350df55405496ab01df91f',
+          storageBucket: 'join-angular-708af.appspot.com',
+          apiKey: 'AIzaSyBuo8gX0nO1BJWLUnj_FOAVxy5mCmpJjZc',
+          authDomain: 'join-angular-708af.firebaseapp.com',
+          messagingSenderId: '953154306638',
+        })
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,17 +3,19 @@ import { Task } from './task';
 import { TaskSummary } from './task-summary';
 
 export class User {
+  id?: string;
   name: string;
   email: string;
   password: string;
-  taskSummary: TaskSummary;
-  tasks: Task[];
-  contacts: Contact[];
+  taskSummary?: TaskSummary;
+  tasks?: Task[];
+  contacts?: Contact[];
 
-  constructor() {
-    this.name = 'name';
-    this.email = 'email';
-    this.password = 'password';
+  constructor(user?: any) {
+    this.id = user ? user.id : '0';
+    this.name = user ? user.name : 'name';
+    this.email = user ? user.email : 'email';
+    this.password = user ? user.password : 'password';
     this.taskSummary = new TaskSummary();
     this.tasks = [new Task()];
     this.contacts = [new Contact()];
