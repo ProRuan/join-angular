1622612e13924f9f6d01400144177d385a478ba8
Your Name <you@example.com>
2024-09-25 22:08:37 +0200

    got due date input completely to run

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -25,6 +25,8 @@ export class AddTaskComponent {
   task = new Task();
   dueDate: any;
   currDate: string = new Date().toLocaleDateString();
+  dateInvalid: boolean = false;
+  dueDatePat = /([0-3]?[0-9])[\.\/]([0-1]?[0-9])[\.\/]([0-9]{4})/;
 
   // Please review!!!
 
@@ -32,7 +34,7 @@ export class AddTaskComponent {
   // description - check
   // assignedTo ...
   // dueDate - semiCheck
-  // icon, single digit (day, month), replace dot ... (0/3)
+  // icon, single digit (day, month), replace dot, hint ... (4/4)
   // prio - check
   // category ...
   // subtasks ...
@@ -61,16 +63,41 @@ export class AddTaskComponent {
     console.log('new Date: ', this.currDate);
   }
 
+  // to improve!!!
   updateCalender() {
-    if (this.task.dueDate.length == 10) {
-      let [day, month, year] = this.task.dueDate.split('/');
-      this.dueDate = year + '-' + month + '-' + day;
-      // console.log('date complete: ', this.dueDate);
-      let validDate = new Date(`${year}/${month}/${day}`);
+    let dateCompleted = this.task.dueDate.match(this.dueDatePat);
+    if (dateCompleted) {
+      console.log('due date pattern: ', dateCompleted);
+      let day = dateCompleted[1];
+      let d = day;
+      if (day.length == 1) {
+        day = '0' + day;
+      }
+      let month = dateCompleted[2];
+      let m = month;
+      if (month.length == 1) {
+        month = '0' + month;
+      }
+      let year = dateCompleted[3];
+      let y = year;
+      this.task.dueDate = `${day}/${month}/${year}`;
+      this.dueDate = `${year}-${month}-${day}`;
+      console.log('got task due date by pattern: ', this.task.dueDate);
+      console.log('got due date by pattern: ', this.dueDate);
+
+      let dateInput = `${parseInt(d)}.${parseInt(m)}.${y}`;
+      let validDate = new Date(`${y}/${m}/${d}`);
+      let dateMatch = dateInput == validDate.toLocaleDateString();
       if (validDate) {
+        console.log('date input: ', dateInput);
         console.log('date validation: ', validDate.toLocaleDateString());
+        console.log('date match: ', dateMatch);
       }
     }
+  }
+
+  showHint() {
+    return this.dateInvalid ? 'o-1' : '';
   }
 
   formatDueDate() {
