1dfee162064d30ab28c672ae3c6b4397d4f240a8
Your Name <you@example.com>
2024-10-21 10:56:13 +0200

    expanded input component by type password

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -3,10 +3,13 @@ import {
   Component,
   EventEmitter,
   forwardRef,
+  inject,
   Input,
   Output,
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ValidationService } from '../../services/validation.service';
+import { PasswordVal } from '../../models/password-val';
 
 @Component({
   selector: 'app-input',
@@ -28,7 +31,12 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
  * @implements - The ControlValueAccessor.
  */
 export class InputComponent implements ControlValueAccessor {
+  val: ValidationService = inject(ValidationService);
+
+  agent: string = '';
+  cache: string = '';
   value: string = '';
+  agentTimeout: any;
   visible: boolean = false;
   @Input() type: string = '';
   @Input() placeholder: string = '';
@@ -72,12 +80,122 @@ export class InputComponent implements ControlValueAccessor {
     this.value = value;
   }
 
+  getAgent() {
+    return this.visible ? 'agent-plus' : '';
+  }
+
   /**
    * Provides the css class.
    * @returns - The css class name.
    */
   getClass() {
     return this.type && !this.visible ? this.type : '';
+  }
+
+  updatePassword(event: KeyboardEvent) {
+    let chars = 'abcdefghijklmnopqrstuvwxyzäöüß';
+    let digits = '0123456789';
+    let special = '!@#$%^&*';
+    const allowedChars = chars + digits + special;
+    const charSet = new Set(allowedChars);
+    let keyboard = event;
+    let key = keyboard.key;
+    let tempKey = key.toLowerCase();
+    console.log('key: ', key);
+    console.log('temp key: ', tempKey);
+    let allowedKeys = [
+      'home',
+      'end',
+      'insert',
+      'delete',
+      'backspace',
+      'tab',
+      'capslock',
+      'shift',
+      'control',
+      'meta',
+      'alt',
+      'altgraph',
+      'arrowleft',
+      'arrowright',
+    ];
+
+    let input = event.target as HTMLInputElement;
+
+    clearTimeout(this.agentTimeout);
+
+    if (!charSet.has(tempKey) && !allowedKeys.includes(tempKey)) {
+      keyboard.preventDefault();
+    } else if (
+      charSet.has(tempKey) ||
+      tempKey == 'backspace' ||
+      tempKey == 'delete'
+    ) {
+      // update confirmed value!!!
+      this.cache = this.value;
+      let newIndex = input.selectionStart ? input.selectionStart : 0;
+      setTimeout(() => {
+        // this.agent = this.value;
+        this.agent = '';
+        for (let i = 0; i < this.value.length; i++) {
+          if (i != newIndex || tempKey == 'backspace' || tempKey == 'delete') {
+            this.agent += `\u25CF`;
+          } else {
+            this.agent += this.value[newIndex];
+          }
+        }
+        console.log('value: ', this.value);
+        console.log('value agent: ', this.agent);
+
+        setTimeout(() => {
+          this.agent = this.agent.replace(this.agent[newIndex], '\u25CF');
+        }, 250);
+      }, 0);
+    }
+  }
+
+  isInvalidPassword() {
+    let password = this.val.getPassword(this.value);
+    let passwordValid = new PasswordVal(this.value).ok;
+    if (password.length < 8 || !passwordValid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getPasswordHint() {
+    // let blackCircle: string = '\u25CF';
+    // console.log(blackCircle); // Output: ●
+    // this.password =
+    //   blackCircle +
+    //   blackCircle +
+    //   blackCircle +
+    //   blackCircle +
+    //   blackCircle +
+    //   blackCircle +
+    //   blackCircle +
+    //   blackCircle;
+
+    if (this.value.length > 7) {
+      if (!/[A-Z]/.test(this.value)) {
+        return 'Use at least 1 capital letter.';
+      } else if (!/[a-z]/.test(this.value)) {
+        return 'Use at least 1 small letter';
+      } else if (!/\d/.test(this.value)) {
+        return 'Use at least 1 digit.';
+      } else {
+        return 'Use at least 1 special character.';
+      }
+    } else {
+      return 'Enter at least 8 characters';
+    }
+  }
+
+  preventCopyPaste(event: Event): void {
+    let clipboard = event as ClipboardEvent;
+    clipboard.clipboardData?.setData('text', '');
+    event.preventDefault();
   }
 
   /**
