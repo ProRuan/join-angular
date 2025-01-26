4cef30132badb4ad47e6077e65256ecea8f92d49
Your Name <you@example.com>
2025-01-27 00:21:57 +0100

    edited contact-viewer template and style

--- src/app/components/contacts/contact-viewer/contact-viewer.component.ts
+++ src/app/components/contacts/contact-viewer/contact-viewer.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ContactService } from '../../../shared/services/contact.service';
+import { ButtonData } from '../../../shared/interfaces/button-data';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-contact-viewer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './contact-viewer.component.html',
   styleUrl: './contact-viewer.component.scss',
 })
@@ -13,8 +15,31 @@ export class ContactViewerComponent {
   // rename?!
   viewer: ContactService = inject(ContactService);
 
-  // add contact phone ...
+  // add onEdit() + onDelete() ... (0/2)
+  // edit-contact dialog ...
+  // delete-contact dialog ...
   // add contact viewer transition ...
+  // ButtonDataService ...
+
+  editBtn: ButtonData = {
+    buttonClass: 'settings-btn',
+    contClass: 'w-31',
+    textClass: 'settings-btn-text',
+    text: 'Edit',
+    imgClass: 'edit',
+    src: '/assets/img/contacts/edit.png',
+    alt: 'edit',
+  };
+
+  deleteBtn: ButtonData = {
+    buttonClass: 'settings-btn',
+    contClass: 'w-49',
+    textClass: 'settings-btn-text',
+    text: 'Delete',
+    imgClass: 'delete',
+    src: '/assets/img/contacts/delete.png',
+    alt: 'delete',
+  };
 
   get contact() {
     return this.viewer.contact;
